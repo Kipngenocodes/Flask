a568df44888cca4d14d7252c08f5a95ae7f483c8
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-02-11 15:38:58 -0800

    adding the telegram bot to the project

---
+++ config.py
@@ -0,0 +1,4 @@
+class Config:
+    TELEGRAM_BOT_TOKEN = "YOUR_TELEGRAM_BOT_TOKEN"
+    TELEGRAM_CHAT_ID = "YOUR_TELEGRAM_CHAT_ID"  # Your personal Telegram ID
+    FLASK_SERVER = "http://your-server.com"  # Change this to your live Flask server URL
