0fbe224cdbdff3258d7c2d55b538e3a8bece1cc8
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-02-11 16:02:58 -0800

    working with the telegram_bot

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config:
-    TELEGRAM_BOT_TOKEN = "YOUR_TELEGRAM_BOT_TOKEN"
-    TELEGRAM_CHAT_ID = "YOUR_TELEGRAM_CHAT_ID"  # Your personal Telegram ID
+    TELEGRAM_BOT_TOKEN = "7674177561:AAGUJnU7tp8XCBkfmmqF8aCd-nuMOrZ2SkU"
+    TELEGRAM_CHAT_ID = "7437393011"  # Your personal Telegram ID
     FLASK_SERVER = "http://your-server.com"  # Change this to your live Flask server URL
